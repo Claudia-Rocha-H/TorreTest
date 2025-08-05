35fca3d33a411329f3a42cde92c3a96a42e12170
Claudia <rochaclaudia177@gmail.com>
2025-08-05 06:57:31 -0500

    feat: Initial project setup for frontend and backend

---
+++ backend/src/main/java/com/torre/techtest/TorreTechTestApplication.java
@@ -0,0 +1,13 @@
+package com.torre.techtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorreTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorreTechTestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/torre/techtest/TorreTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torre.techtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorreTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
